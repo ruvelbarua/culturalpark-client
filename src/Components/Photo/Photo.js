46990fa968ea911f43c94bdb8f4f9cda93525a51
ruvelbarua <ruvel.br78@gmail.com>
2022-03-20 23:55:31 +0600

    update

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -2,16 +2,13 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 const Photo = (props) => {
-    const { name, description, img } = props.photo || {}
+    const { name, img } = props.photo || {}
     return (
         <div className="col-md-4">
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" className='img-fluid img-thumbnail d-block m-auto' src={img} />
                 <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>
-                        {description}
-                    </Card.Text>
+                    <Card.Title className='text-center'>{name}</Card.Title>
                 </Card.Body>
             </Card>
         </div>
