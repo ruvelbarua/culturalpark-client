5186882f8ae1df7501e87c2f9284cdd50f45a42f
ruvelbarua <ruvel.br78@gmail.com>
2022-06-10 08:56:03 +0600

    update

--- src/Components/Media/Media.js
+++ src/Components/Media/Media.js
@@ -4,12 +4,10 @@ import ReactPlayer from 'react-player'
 const Media = () => {
     return (
         <div className='d-flex gap-4 m-4'>
-
             <ReactPlayer url='https://youtu.be/fiUbAJ5zbU8' />
-
-
             <ReactPlayer url='https://youtu.be/2G9BmYDnZuQ' />
-
+            <ReactPlayer url='https://youtu.be/euRy3312cRk' />
+            <ReactPlayer url='https://youtu.be/1A1IOKBecJU' />
         </div>
     );
 };
